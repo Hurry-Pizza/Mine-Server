950e791a50a96e120673e95a5a371ce2dd191b19
KimDoubleB <tree9295@gmail.com>
2022-08-20 04:22:52 +0900

    inital commit

---
+++ src/main/java/com/hurrypizza/digda/DigdaApplication.java
@@ -0,0 +1,13 @@
+package com.hurrypizza.digda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hurrypizza/digda/DigdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hurrypizza.digda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
