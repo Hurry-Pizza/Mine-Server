9d5a813f73ce1a83a2f9913dd3201b31664f8d9a
KimDoubleB <tree9295@gmail.com>
2022-08-20 14:14:56 +0900

    feat: Email validation

--- src/main/java/com/hurrypizza/digda/api/v1/dto/UserRegisterRequest.java
+++ src/main/java/com/hurrypizza/digda/api/v1/dto/UserRegisterRequest.java
@@ -3,6 +3,7 @@ package com.hurrypizza.digda.api.v1.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
@@ -10,6 +11,7 @@ import javax.validation.constraints.Pattern;
 @Getter
 public class UserRegisterRequest {
 
+    @Email
     private String email;
 
     @NotEmpty(message = "필수 입력 항목입니다.")
