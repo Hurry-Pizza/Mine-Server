3957d39f8e7a8d9430a66cddf3bbfb2817f6c847
KimDoubleB <tree9295@gmail.com>
2022-08-20 22:57:09 +0900

    feat: login validate endpoint

--- src/main/java/com/hurrypizza/mine/api/v1/UserAuthController.java
+++ src/main/java/com/hurrypizza/mine/api/v1/UserAuthController.java
@@ -4,6 +4,7 @@ import com.hurrypizza.mine.api.ApiResponse;
 import com.hurrypizza.mine.api.v1.dto.UserRegisterRequest;
 import com.hurrypizza.mine.config.security.token.TokenProvider;
 import com.hurrypizza.mine.config.security.token.jwt.JwtTokenSupport;
+import com.hurrypizza.mine.config.security.util.SecurityUtils;
 import com.hurrypizza.mine.domain.user.UserAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -11,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +40,14 @@ public class UserAuthController {
         return ResponseEntity.status(HttpStatus.CREATED)
                        .header(HttpHeaders.AUTHORIZATION, JwtTokenSupport.tokenWithType(token))
                        .body(ApiResponse.emptyResponse());
+    }
+
+    @GetMapping("/validate")
+    public ResponseEntity<ApiResponse<String>> validateLogin() {
+        if (SecurityUtils.isAuthenticated()) {
+            return ResponseEntity.ok().body(ApiResponse.emptyResponse());
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.emptyResponse());
     }
 
 }
